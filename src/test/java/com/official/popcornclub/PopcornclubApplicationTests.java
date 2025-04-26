1bee829a1a9b3d3404bc45e9b7fdc0993fce174c
Lucas-Viesan <vieiratm01@hotmail.com>
2025-04-26 15:24:00 -0300

    Instaladas dependencias e configuraçoes no banco de dados

---
+++ src/test/java/com/official/popcornclub/PopcornclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.official.popcornclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopcornclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
