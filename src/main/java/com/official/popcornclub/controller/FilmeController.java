4b4e85bf79ba5725d8fd174c600ea8f5dcef5287
Lucas-Viesan <vieiratm01@hotmail.com>
2025-04-27 18:00:16 -0300

    Implementado e testado CRUD completo da entidade filme

--- src/main/java/com/official/popcornclub/controller/FilmeController.java
+++ src/main/java/com/official/popcornclub/controller/FilmeController.java
@@ -1,17 +1,22 @@
 package com.official.popcornclub.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.official.popcornclub.model.Filme;
 import com.official.popcornclub.repository.FilmeRepository;
@@ -45,6 +50,26 @@ public class FilmeController {
 	public ResponseEntity<Filme> post(@Valid @RequestBody Filme filme){
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(filmeRepository.save(filme));
+	}
+	
+	@PutMapping("/atualizar")
+	public ResponseEntity<Filme> put(@Valid @RequestBody Filme filme){
+		return filmeRepository.findById(filme.getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
+						.body(filmeRepository.save(filme)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Filme> postagem = filmeRepository.findById(id);
+		
+		if(postagem.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		} else {
+			filmeRepository.deleteById(id);
+		}
 	}
 
 }
